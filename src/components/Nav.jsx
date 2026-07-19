35771dce73c757d41daee776592b9a70c6a168c4
jefferykushner <jeffery.kushner@gmail.com>
2026-07-19 08:56:07 -0400

    Add files via upload

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,30 @@
 import { Link, useLocation } from 'react-router-dom'
+import { useBrand } from '../brand/BrandContext'
 import './Nav.css'
 
 export default function Nav() {
   const { pathname } = useLocation()
+  const brand = useBrand()
   const isTsismis = pathname.startsWith('/tsismis')
 
   return (
     <nav className={`nav ${isTsismis ? 'nav--tsismis' : ''}`} aria-label="Site navigation">
-      <Link to="/" className="nav__brand">
-        Love in My Tummy
+      <Link to={isTsismis ? '/tsismis' : '/'} className="nav__brand">
+        {brand.label}
       </Link>
 
       <ul className="nav__links">
-        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to={isTsismis ? '/tsismis/menu' : '/menu'}>Menu</Link></li>
         <li><Link to="/story">Our Story</Link></li>
-        <li><Link to="/tsismis" className="nav__link--tsismis">Tsismis ✦</Link></li>
+        {isTsismis ? (
+          <li><Link to="/" className="nav__link--limt">Love in My Tummy</Link></li>
+        ) : (
+          <li><Link to="/tsismis" className="nav__link--tsismis">Tsismis ✦</Link></li>
+        )}
         <li><Link to="/shop">LG Shop</Link></li>
       </ul>
 
-      <Link to="/order" className="btn btn--blush nav__order">
+      <Link to="/order" className={`btn nav__order ${isTsismis ? 'btn--ube' : 'btn--blush'}`}>
         Order Now
       </Link>
     </nav>
